a547f70b7601447666f0ab6d1ca3b3599021b519
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2022-02-03 17:41:26 +0300

    Update checkCashRegister.js

--- checkCashRegister.js
+++ checkCashRegister.js
@@ -71,5 +71,5 @@ function checkCashRegister(price, cash, cid) {
 
   return {status: "OPEN",change: newChange};
 }
-
+//Example Case
 console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
