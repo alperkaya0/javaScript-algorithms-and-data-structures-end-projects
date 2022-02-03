e5b164affa9f2f22005741e826a8f7b7465cc00e
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2022-02-03 17:01:52 +0300

    Update caesars-cipher.js

--- caesars-cipher.js
+++ caesars-cipher.js
@@ -6,7 +6,7 @@ function rot13(str) {
   for (let i = 0; i < str.length; ++i) {
     if (/[\w]/.test(str[i])) {//check if the char consists of alphanumeric elements
       let idx = alp.indexOf(str[i])-13;//find the alphabet place of the char and return the element 13 before
-      if (idx < 0) {//you may think this as we are making alp array a linked list that shapes a circle (the element after the last element is the first element)
+      if (idx < 0) {//you may think this as we are making alp array like a linked list that shapes a circle (the element after the last element is the first element)
         idx += alp.length;
       }
       res += alp[idx];//building our result char by char
