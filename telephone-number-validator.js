57fe2186996ce4d4b296c8800cebea1d5ddeda6c
Alper Kaya <73350743+alperkaya0@users.noreply.github.com>
2022-02-03 17:40:55 +0300

    Update telephone-number-validator.js

--- telephone-number-validator.js
+++ telephone-number-validator.js
@@ -1,4 +1,5 @@
 function telephoneCheck(str) {
+  //All of these are the specified regexes by the question
   let regex = /^\d{3}-\d{3}-\d{4}$/;
   let regex2 = /^\(\d{3}\)\d{3}-\d{4}$/;
   let regex3 = /^\(\d{3}\) \d{3}-\d{4}$/;
@@ -8,7 +9,8 @@ function telephoneCheck(str) {
   let regex7 = /^1 \d{3}-\d{3}-\d{4}/;
   let regex8 = /^1 \(\d{3}\) \d{3}-\d{4}/;
   let regex9 = /^1\(\d{3}\)\d{3}-\d{4}/;
+  //Test all of them and return true if any of them passes.
   return regex.test(str) || regex2.test(str) || regex3.test(str) || regex4.test(str) || regex5.test(str) || regex6.test(str) || regex7.test(str) || regex8.test(str) || regex9.test(str);
 }
 
-console.log(telephoneCheck("1 (555) 555-5555"));
+console.log(telephoneCheck("1 (555) 555-5555"));//Example
